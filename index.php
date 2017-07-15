84e0a1f9c5fbff1e4b08ad30bed593b355a4a4fa
WOoOinux <contact@woinux.fr>
2017-07-15 23:30:14 +0200

    Add race template

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8" />
+            <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css">
+            <link rel="stylesheet" type="text/css" href="style.css" />
+            <title>Hack::Ardennes - GIT GAME</title>
+        </head>
+        <body>
+            <header>
+                <div class="gg-logo"></div>
+                <div class="gg-title"></div>
+            </header>
+            <section class="gg-statistics">
+                <!-- To do... -->
+            </section>
+            <section class="gg-race">
+                <div class="gg-racer" style="width: 100%;"></div>
+                <div class="gg-racer" style="width: 80%;"></div>
+                <div class="gg-racer" style="width: 60%;"></div>
+                <div class="gg-racer" style="width: 40%;"></div>
+                <div class="gg-racer" style="width: 20%;"></div>
+                <div class="gg-racer" style="width: 0%;"></div>
+            </section>
+            <section class="gg-mountain"></section>
+        </body>
+    </html>
