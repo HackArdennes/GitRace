a194346f992078c88536c386fab3de8db465cc61
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2017-08-15 10:28:12 +0200

    Add social buttons

--- index.php
+++ index.php
@@ -7,6 +7,14 @@
             <title>Hack::Ardennes - The Git Race</title>
         </head>
         <body>
+            <div id="fb-root"></div>
+            <script>(function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.10";
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));</script>
             <?php
                 include("gitracelib.php");
                 include("config.php");
@@ -59,12 +67,15 @@
                 </div>
             </section>
             <section>
-                <div class="gg-twitter">
+                <div class="gg-social">
                     <p>
                         <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://thegitrace.hackardennes.com/" data-text="Course aux commits au #hackathon @CabaretVert ! @github #HackCV17 #DD #OSS" data-via="HackArdennes" data-lang="fr" data-size="large">Tweeter</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+                        &nbsp;
+                        <div class="fb-share-button" data-href="http://thegitrace.hackardennes.com/" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fthegitrace.hackardennes.com%2F&amp;src=sdkpreparse">Partager</a></div>
                     </p>
                 </div>
+
             </section>
             <section class="gg-race">
                 <?php
