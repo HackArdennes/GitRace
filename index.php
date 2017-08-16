2ac0037512518499265392b98e98834b72bbef4f
WOoOinux <contact@woinux.fr>
2017-08-16 20:18:13 +0200

    Responsive rendering

--- index.php
+++ index.php
@@ -5,22 +5,24 @@
             <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css">
             <link rel="stylesheet" type="text/css" href="style.css" />
             <title>Hack::Ardennes - The Git Race</title>
-             <meta http-equiv="refresh" content="60; URL=http://localhost/GitGame/">
+            <meta http-equiv="refresh" content="60; URL=http://localhost/GitGame/">
         </head>
         <body>
             <div id="fb-root"></div>
-            <script>(function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s); js.id = id;
-              js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.10";
-              fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));</script>
+            <script>
+                (function(d, s, id) {
+                  var js, fjs = d.getElementsByTagName(s)[0];
+                  if (d.getElementById(id)) return;
+                  js = d.createElement(s); js.id = id;
+                  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.10";
+                  fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));
+            </script>
             <?php
                 include("gitracelib.php");
-                include("config.php");
+                //include("config.php");
 
-                $prod = true;
+                $prod = false;
                 $branch = "master";
                 $deadline = "20170827170000";
                 $currentdatetime = date("YmdHis");
@@ -67,16 +69,13 @@
                     </p>
                 </div>
             </section>
-            <section>
-                <div class="gg-social">
-                    <p>
-                        <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://thegitrace.hackardennes.com/" data-text="Course aux commits au #hackathon @CabaretVert ! @github #HackCV17 #DD #OSS" data-via="HackArdennes" data-lang="fr" data-size="large">Tweeter</a>
+            <section class="gg-social">
+                <p>
+                    <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://thegitrace.hackardennes.com/" data-text="Course aux commits au #hackathon @CabaretVert ! @github #HackCV17 #DD #OSS" data-via="HackArdennes" data-lang="fr" data-size="large">Tweeter</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-                        &nbsp;
-                        <div class="fb-share-button" data-href="http://thegitrace.hackardennes.com/" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fthegitrace.hackardennes.com%2F&amp;src=sdkpreparse">Partager</a></div>
-                    </p>
-                </div>
-
+                    &nbsp;
+                    <div class="fb-share-button" data-href="http://thegitrace.hackardennes.com/" data-layout="button" data-size="large" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fthegitrace.hackardennes.com%2F&amp;src=sdkpreparse">Partager</a></div>
+                </p>
             </section>
             <section class="gg-race">
                 <?php
